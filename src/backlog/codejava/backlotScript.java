0435a6e84e6f9dc5129c0a853b8cfe1d499dffa9
Edgar <edgar.ochoa.c@gmail.com>
2018-05-13 18:40:36 +0200

    Update backlotScript.java

--- src/backlog/codejava/backlotScript.java
+++ src/backlog/codejava/backlotScript.java
@@ -8,7 +8,7 @@ public class backlotScript {
 		String path ="/v3/analytics/reports/";//i.e. /v2/assets/{embed)code}/metadata
 		String expires ="1580569200";//set a mnaual expiration time or use getExpiration()
 		String requestBody =""; //the JSON body should be like this one {\"created_at\":\"should works\"}
-		backlotAPI sign = new backlotAPI("ZzZGMxOo_dNZEF6BH2MfXVqweXLU.4918i","hD5obJ7TxBwzN1MQcZHAtjZQtM0tUWM-7OL1Dyzt");
+		backlotAPI sign = new backlotAPI("ZzZGMxOo_dNZEF6BH2MfXVqweXLU.4918i","");
 		HashMap<String, String> parameters = new HashMap<String, String>();
 		parameters.put("report_type", "performance");
 		//parameters.put("dimensions", "asset");
